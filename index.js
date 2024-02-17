a03c4d3202cf370ab782a4aefe179151b8c89489
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2024-02-17 11:22:32 +0000

    INDEX - Express started - Listening for requests

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require("dotenv").config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 
 const {
   checkDB,
@@ -7,9 +10,34 @@ const {
 
 const addRelations = require('./database/relations')
 
+const connectToDB = async () => {
+  await checkDB();
+  addRelations();
+  await syncModels();
+}
+
+const startExpress = () => {
+  try {
+    const app = express()
+
+    app.use(cors())
+    app.use(morgan('dev'))
+    app.use(express.json())
+
+    app.get('/api', (req, res) => {
+      res.send('Request received succesfuly')
+    })
+  
+    app.listen(process.env.PORT, () => {
+      console.log(`Express started. Listening on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    throw new Error (error)
+  }
+
+}
 
 ;(async () => {
-  await checkDB()
-  addRelations()
-  syncModels()
+  await connectToDB()
+  startExpress()
 })()
